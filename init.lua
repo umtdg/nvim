9f2829d55f56a2824c3410240faa223462bf1549
Umut Dag <umu7d9@gmail.com>
2025-02-07 11:12:58 +0300

    chore: switch to dashboard-nvim and add persistence.nvim

--- init.lua
+++ init.lua
@@ -979,6 +979,7 @@ require('lazy').setup({
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   require 'custom.plugins.intro',
   require 'custom.plugins.bufferline',
+  require 'custom.plugins.persistence',
   -- { import = 'custom.plugins' },
   --
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
