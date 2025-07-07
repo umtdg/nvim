8e18d9e56a65c0c27646ea0b93c2d7c78daae97c
Umut Dag <umu7d9@gmail.com>
2025-07-07 14:27:39 +0300

    feat(java): disable format on save for java

--- init.lua
+++ init.lua
@@ -792,7 +792,7 @@ require('lazy').setup({
         -- Disable "format_on_save lsp_fallback" for languages that don't
         -- have a well standardized coding style. You can add additional
         -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true, cpp = true, bash = true, rust = true, go = true, python = true }
+        local disable_filetypes = { c = true, cpp = true, bash = true, rust = true, go = true, python = true, java = true }
         if disable_filetypes[vim.bo[bufnr].filetype] then
           return nil
         else
@@ -808,6 +808,7 @@ require('lazy').setup({
         c = { 'clang-format' },
         cpp = { 'clang-format' },
         html = { 'prettier' },
+        java = { 'jdtls' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
