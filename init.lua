469e3d9cc4b87476f104db044b228875133f0955
Umut Dag <umu7d9@gmail.com>
2025-09-11 14:04:41 +0300

    fix: update lsp server configuration to work with mason-lspconfig 2.0

--- init.lua
+++ init.lua
@@ -753,20 +753,13 @@ require('lazy').setup({
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
+      for server_name, server_settings in pairs(servers) do
+        require('lspconfig')[server_name].setup { settings = server_settings }
+      end
+
       require('mason-lspconfig').setup {
         ensure_installed = {}, -- explicitly set to an empty table (Kickstart populates installs via mason-tool-installer)
         automatic_installation = false,
-        -- automatic_enable = {},
-        handlers = {
-          function(server_name)
-            local server = servers[server_name] or {}
-            -- This handles overriding only values explicitly passed
-            -- by the server configuration above. Useful when disabling
-            -- certain features of an LSP (for example, turning off formatting for ts_ls)
-            server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
-            require('lspconfig')[server_name].setup(server)
-          end,
-        },
       }
     end,
   },
