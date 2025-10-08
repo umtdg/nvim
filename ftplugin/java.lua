8c6dccaf0e333ba1f04dc1dcb30552f770356dfe
Umut Dag <umu7d9@gmail.com>
2025-10-08 08:16:13 +0300

    chore: remove java 8, 11, and 24 from jdtls config

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -54,11 +54,8 @@ local config = {
 
       configuration = {
         runtimes = {
-          { name = 'JavaSE-8', path = '/usr/lib/jvm/java-8-openjdk' },
           { name = 'JavaSE-11', path = '/usr/lib/jvm/java-11-openjdk' },
-          { name = 'JavaSE-17', path = '/usr/lib/jvm/java-17-openjdk' },
           { name = 'JavaSE-21', path = '/usr/lib/jvm/java-21-openjdk' },
-          { name = 'JavaSE-24', path = '/usr/lib/jvm/java-24-openjdk' },
         },
       },
 
