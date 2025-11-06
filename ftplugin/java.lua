80a52d552ddd0f40d5ee1db1faab639f9f1b6af0
Umut Dag <umu7d9@gmail.com>
2025-11-06 07:32:58 +0300

    feat: configure jdtls for use with dap

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -14,6 +14,7 @@ end
 
 local data_path = vim.fn.stdpath 'data'
 
+-- jdtls paths
 local jdtls_path = data_path .. '/mason/packages/jdtls'
 local jdtls_jar = vim.fn.glob(jdtls_path .. '/plugins/org.eclipse.equinox.launcher_*.jar')
 local ws_dir = data_path .. '/jdtls-workspace/' .. vim.fn.fnamemodify(root_dir, ':p:h')
@@ -71,6 +72,28 @@ local config = {
   init_options = {
     bundles = {},
   },
+}
+
+-- configuration for debugging
+local bundles = {
+  vim.fn.glob(data_path .. '/mason/packages/java-debug-adapter/extension/server/com.microsoft.java.debug.plugin-*.jar'),
+}
+
+local java_test_bundles = vim.fn.glob(data_path .. '/mason/packages/java-test/extension/server/*.jar', false, true)
+local java_test_bundles_exclude = {
+  'com.microsoft.java.test.runner-jar-with-dependencies.jar',
+  'jacocoagent.jar',
+}
+
+for _, java_test_jar in ipairs(java_test_bundles) do
+  local fname = vim.fn.fnamemodify(java_test_jar, ':t')
+  if not vim.tbl_contains(java_test_bundles_exclude, fname) then
+    table.insert(bundles, java_test_jar)
+  end
+end
+
+config.init_options = {
+  bundles = bundles,
 }
 
 jdtls.start_or_attach(config)
--- lua/custom/plugins/jdtls.lua
+++ lua/custom/plugins/jdtls.lua
@@ -1,3 +1,24 @@
 return {
   'mfussenegger/nvim-jdtls',
+  dependencies = {
+    {
+      'nvim-dap',
+      keys = {
+        {
+          '<leader>dt',
+          function()
+            require('jdtls').test_class()
+          end,
+          desc = 'Debug: Test current class',
+        },
+        {
+          '<leader>df',
+          function()
+            require('jdtls').test_nearest_method()
+          end,
+          desc = 'Debug: Test nearest function',
+        },
+      },
+    },
+  },
 }
