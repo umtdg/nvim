09890dbe55e40db751c5e065db1ef7545f30066c
Umut Dag <umu7d9@gmail.com>
2025-11-27 13:49:32 +0300

    fix: treesitter module config

--- lua/umtdg/lazy/treesitter.lua
+++ lua/umtdg/lazy/treesitter.lua
@@ -1,4 +1,5 @@
 ---@type LazyPluginSpec
+---@diagnostic disable: missing-fields
 return {
   'nvim-treesitter/nvim-treesitter',
   branch = 'master',
@@ -6,21 +7,18 @@ return {
   main = 'nvim-treesitter.configs',
   ---@module 'nvim-treesitter'
   ---@type TSConfig
-  ---@diagnostic disable-next-line: missing-fields
   opts = {
     ensure_installed = { 'bash', 'lua', 'vim' },
     auto_install = true,
-    modules = {
-      ---@diagnostic disable-next-line: missing-fields
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = { 'rust' },
-      },
-      ---@diagnostic disable-next-line: missing-fields
-      indent = {
-        enable = true,
-        disable = { 'rust' },
-      },
+    ---@type TSModule
+    highlight = {
+      enable = true,
+      additional_vim_regex_highlighting = { 'rust' },
+    },
+    ---@type TSModule
+    indent = {
+      enable = true,
+      disable = { 'rust' },
     },
   },
 }
