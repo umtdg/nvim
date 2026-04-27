34143cf5662ec72f0fa9368a750ba0c7020dcd9c
Umut Dag <me@umtdg.com>
2026-04-27 15:29:36 +0300

    conform(biome): use `biome-check` instead of `biome` for sorting imports too

--- lua/umtdg/lazy/conform.lua
+++ lua/umtdg/lazy/conform.lua
@@ -12,10 +12,10 @@ return {
       cpp = { 'clang-format' },
       java = { 'jdtls' },
       nix = { 'nixfmt' },
-      javascript = { 'biome', 'prettierd', 'prettier' },
-      typescript = { 'biome', 'prettierd', 'prettier' },
-      javascriptreact = { 'biome', 'prettierd', 'prettier' },
-      typescriptreact = { 'biome', 'prettierd', 'prettier' },
+      javascript = { 'biome-check', 'prettierd', 'prettier', stop_after_first = true },
+      typescript = { 'biome-check', 'prettierd', 'prettier', stop_after_first = true },
+      javascriptreact = { 'biome-check', 'prettierd', 'prettier', stop_after_first = true },
+      typescriptreact = { 'biome-check', 'prettierd', 'prettier', stop_after_first = true },
     },
     formatters = {
       black = {
