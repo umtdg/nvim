3d63eda12c28b1ab46ec1d21187a7008a1bfd04b
Umut Dag <me@umtdg.com>
2026-07-14 08:02:34 +0300

    feat(overseer): add shortcuts and restart keybind

--- lua/umtdg/lazy/overseer.lua
+++ lua/umtdg/lazy/overseer.lua
@@ -4,5 +4,20 @@ return {
 
   ---@module 'overseer'
   ---@type overseer.SetupOpts
-  opts = {},
+  opts = {
+    task_list = {
+      direction = 'bottom',
+      keymaps = {
+        ['r'] = { 'keymap.run_action', opts = { action = 'restart' }, desc = 'Restart task' },
+      },
+    },
+  },
+  config = function(_, opts)
+    local overseer = require 'overseer'
+    overseer.setup(opts)
+
+    vim.keymap.set('n', '<leader>ot', overseer.toggle, { desc = '[O]verseer [T]oggle' })
+    vim.keymap.set('n', '<leader>or', ':OverseerRun<CR>', { desc = '[O]verseer [R]un' })
+    vim.keymap.set('n', '<leader>os', ':OverseerShell<CR>', { desc = '[O]verseer [S]hell' })
+  end,
 }
